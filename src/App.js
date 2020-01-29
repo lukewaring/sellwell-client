eda03ea2cb53ae0d6b693d9036cc84af60390738
lukewaring <lukewaring@gmail.com>
2020-01-29 16:18:27 -0500

    Render accounts in DetailsContaier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DetailsContainer from './Containers/DetailsContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  state = {
+    accounts: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/accounts')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        accounts: data
+      })
+    })
+  }
+
+  render() {
+    console.log('App state', this.state)
+    return (
+      <div className="App">
+        <DetailsContainer accounts={this.state.accounts} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Containers/DetailsContainer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const DetailsContainer = (props) => {
+    
+    console.log('DetailsContainer props', props)
+    
+    return (
+        <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+            <TableHead>
+            <TableRow>
+                <TableCell align="left">Name</TableCell>
+                <TableCell align="left">Industry</TableCell>
+                <TableCell align="left">Website</TableCell>
+                <TableCell align="left">Notes</TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            {props.accounts.map(account => (
+                <TableRow key={account.id}>
+                <TableCell component="th" scope="row">{account.name}</TableCell>
+                <TableCell align="left">{account.industry}</TableCell>
+                <TableCell align="left">{account.website}</TableCell>
+                <TableCell align="left">{account.notes}</TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+        </TableContainer>
+    )
+
+}
+
+export default DetailsContainer;
