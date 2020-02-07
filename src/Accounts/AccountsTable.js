8a7c6330ca5b6070e75733954b0cd5ff750407c5
lukewaring <lukewaring@gmail.com>
2020-02-07 12:26:49 -0500

    Switched AccountsTable back to standard Material-UI table (href dynamic id issue)

--- src/Accounts/AccountsTable.js
+++ src/Accounts/AccountsTable.js
@@ -1,20 +1,14 @@
 import React from 'react'
-// import { makeStyles } from '@material-ui/core/styles'
-// import Table from '@material-ui/core/Table'
-// import TableBody from '@material-ui/core/TableBody'
-// import TableCell from '@material-ui/core/TableCell'
-// import TableContainer from '@material-ui/core/TableContainer'
-// import TableHead from '@material-ui/core/TableHead'
-// import TableRow from '@material-ui/core/TableRow'
-// import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 import Button from '@material-ui/core/Button'
 import Fab from '@material-ui/core/Fab'
 import AddIcon from '@material-ui/icons/Add'
-import MaterialTable from 'material-table'
-import AccountShow from './AccountShow'
-import AccountForm from './AccountForm'
-import { Switch, Route } from 'react-router-dom'
-import AccountEditForm from './AccountEditForm'
 
 class AccountsTable extends React.Component {
 
@@ -37,83 +31,42 @@ class AccountsTable extends React.Component {
         })
     }
 
-    acctsArr = []
-
-    getMaterialTableData = () => {
-        this.state.accounts.map(account => {
-            this.acctsArr.push({account: account.name, industry: account.industry, website: account.website, notes: account.notes})
-        })
-        return this.acctsArr
-    }
-
     render() {
     
         return (
             <div>
-            <h2>Accounts</h2>
+            <h2 style={{ textAlign: 'center' }}>Accounts</h2>
             
             <Fab className='add-btn' onClick={() => this.nextPath('/accounts/new') } color="secondary" aria-label="add">
                 <AddIcon />
             </Fab>
-
-            <Switch>
-
-            <Route path="/accounts/new" render={(routerProps) => <AccountForm routerProps={routerProps} />} />
-
-            <Route path="/accounts" render={() => 
-                // <TableContainer component={Paper}>
-                // <Table aria-label="simple table">
-                //     <TableHead>
-                //     <TableRow>
-                //         <TableCell align="left">Account</TableCell>
-                //         <TableCell align="left">Industry</TableCell>
-                //         <TableCell align="left">Website</TableCell>
-                //         <TableCell align="left">Notes</TableCell>
-                //     </TableRow>
-                //     </TableHead>
-                //     <TableBody>
-                //     {this.state.accounts.map(account => (
-                //     <TableRow  key={account.id}>
-                //         <TableCell component="th" scope="row">{account.name}</TableCell>
-                //         <TableCell align="left">{account.industry}</TableCell>
-                //         <TableCell align="left">{account.website}</TableCell>
-                //         <TableCell align="left">{account.notes}</TableCell>
-                //         <Button variant="contained" color="primary" href={`/accounts/${account.id}`}>View</Button>
-                //     </TableRow>
-                //     ))}
-                //     </TableBody>
-                // </Table>
-                // </TableContainer>
-
-                <MaterialTable
-                    title=""
-                    columns={[
-                    { title: 'Account', field: 'account' },
-                    { title: 'Industry', field: 'industry' },
-                    { title: 'Website', field: 'website' },
-                    { title: 'Notes', field: 'notes' },
-                    { title: 'View', field: 'view', render: () => <Button variant="contained" color="primary" href={`/accounts/1`}>View</Button> }
-                    ]}        
-                    
-                    data={this.getMaterialTableData()}
-
-                    options={{
-                    sorting: true
-                    }}
-                />
-
-            }
-            />
-
-            <Route path="/accounts/:id" render={(routerProps) => <AccountShow routerProps={routerProps} accounts={this.state.accounts} />} />
-
-            <Route path="/accounts/:id/edit" render={(routerProps) => <AccountEditForm routerProps={routerProps} accounts={this.state.accounts} />} />
-
-            </Switch>
-
+            
+                <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell style={{ fontWeight: 'bold' }} align="left">Name</TableCell>
+                        <TableCell style={{ fontWeight: 'bold' }} align="left">Industry</TableCell>
+                        <TableCell style={{ fontWeight: 'bold' }} align="left">Website</TableCell>
+                        <TableCell style={{ fontWeight: 'bold' }} align="left">Notes</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {this.state.accounts.map(account => (
+                    <TableRow key={account.id}>
+                        <TableCell component="th" scope="row">{account.name}</TableCell>
+                        <TableCell align="left">{account.industry}</TableCell>
+                        <TableCell align="left">{account.website}</TableCell>
+                        <TableCell align="left">{account.notes}</TableCell>
+                        <Button variant="contained" color="primary" href={`/accounts/${account.id}`}>View</Button>
+                    </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+                </TableContainer>
             </div>
         )
     }
 }
 
-export default AccountsTable;
+export default AccountsTable
