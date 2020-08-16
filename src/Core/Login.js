4d136a263a0e5f34c25a4b610685b9747c1a8f99
lukewaring <lukewaring@gmail.com>
2020-08-16 13:08:31 -0400

    Add fetch post to handleSubmit in login component; add storeToken method

--- src/Core/Login.js
+++ src/Core/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Login extends React.Component {
     state = {
       email: '',
-      password_digest: ''
+      password: ''
     }
 
     handleChange = (event) => {
@@ -14,19 +14,33 @@ class Login extends React.Component {
 
     handleSubmit = (event) => {
       event.preventDefault()
-      if (this.state.password_digest === '123') {
-        this.props.history.push('/accounts')
-      } else {
-        alert('Please enter the correct password.')
-      }
+      
+      fetch(`http://localhost:3001/api/v1/login?email=${this.state.email}&password=${this.state.password}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+      })
+      .then(res => res.json())
+      .then(authObj => this.checkForToken(authObj))
+    }
+
+    checkForToken = (authObj) => {
+      authObj.token ? this.storeToken(authObj) : alert('Please enter the correct password.')
+    }
+
+    storeToken = (authObj) => {
+      localStorage.setItem('token', authObj.token)
+      this.props.history.push('/')
     }
 
     render () {
       return (
         <div>
+          <h1>Login</h1>
           <form onSubmit={this.handleSubmit}>
-            <input name='email' value={this.state.email} onChange={this.handleChange} placeholder='Email' />
-            <input name='password_digest' type='password' value={this.state.password_digest} onChange={this.handleChange} placeholder='Password' />
+            <input type='text' name='email' value={this.state.email} onChange={this.handleChange} placeholder='Email' />
+            <br></br>
+            <input type='password' name='password' value={this.state.password} onChange={this.handleChange} placeholder='Password' />
+            <br></br>
             <button type='submit'>Submit</button>
           </form>
         </div>
